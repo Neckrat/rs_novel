248dd497729176ef8c852500d37a653e26b2af4d
Neckrat <dangerfish547@gmail.com>
2023-01-12 07:44:52 +0300

    dialog state system

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use egui::{RichText, Color32};
 
 fn main() {
     App::new()
+        .init_resource::<DialogState>()
         .add_startup_system(setup)
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             window: WindowDescriptor {
@@ -21,29 +22,38 @@ fn main() {
         .run();
 }
 
-fn setup(mut commands: Commands, _asset_server: Res<AssetServer>) {
+#[derive(Resource, Default)]
+struct DialogState {
+    name: usize,
+    dialog: usize,
+}
 
+fn setup(mut commands: Commands, _asset_server: Res<AssetServer>, mut dialog_state: ResMut<DialogState>) {
+    dialog_state.name = 0;
+    dialog_state.dialog = 0;
     commands.spawn(Camera2dBundle::default());
-
     commands.spawn(SpriteBundle {
         texture: _asset_server.load("backgrounds/scene01.png"),
         ..default()
     });
 }
 
-fn dialog_box(keyboard_input: Res<Input<KeyCode>>, mut egui_context: ResMut<EguiContext>) {
-    let mut name = ["Иван", "Анатолий", "Василий"];
-    let mut dialog = ["Заходит как-то улитка в бар...", "...и говорит бармену:", "Можно мне виски?"];
+fn dialog_box(keyboard_input: Res<Input<KeyCode>>, mut egui_context: ResMut<EguiContext>, mut dialog_state: ResMut<DialogState>) {
+    let name = ["Иван", "Анатолий", "Василий"];
+    let dialog = ["Заходит как-то улитка в бар...", "...и говорит бармену:", "Можно мне виски?"];
     let my_frame = egui::containers::Frame {
         inner_margin: egui::style::Margin { left: 10., right: 10., top: 10., bottom: 10. },
         rounding: egui::Rounding { nw: 1.0, ne: 1.0, sw: 1.0, se: 1.0 },
         fill: Color32::from_black_alpha(224),
         ..Default::default()
     };
+    if keyboard_input.just_pressed(KeyCode::Space) && dialog_state.name < name.len() - 1 && dialog_state.dialog < dialog.len() - 1 {
+        dialog_state.name += 1;
+        dialog_state.dialog += 1;
+    }
     egui::TopBottomPanel::bottom("").frame(my_frame).show(egui_context.ctx_mut(), |ui| {
-        ui.label(RichText::new(name[0]).heading().size(30.0).color(Color32::from_white_alpha(192)));
+        ui.label(RichText::new(name[dialog_state.name]).heading().size(30.0).color(Color32::from_white_alpha(192)));
         ui.separator();
-        ui.label(RichText::new(dialog[0]).size(20.0).color(Color32::from_white_alpha(192)));
+        ui.label(RichText::new(dialog[dialog_state.dialog]).size(20.0).color(Color32::from_white_alpha(192)));
     });
-
 }
